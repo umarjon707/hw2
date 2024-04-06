3d33feda8db0a6ddc75c5727e92d78ffa71c0040
Umarjon <urr777777@gmail.com>
2024-04-06 16:22:11 +0500

    done

---
+++ hw2/index.js
@@ -0,0 +1,23 @@
+let username = prompt("What is your name");
+let money=10000
+
+if (username==="Alex") { 
+ let account = prompt("Hello, Alex! type your password:");
+
+if (account === '7777') {
+    let money=prompt("cash?")
+  
+if (money) {
+    
+    alert("Good!!!!")
+}
+else {
+    alert("your money is not enough!!!") 
+}
+ } else {
+alert("get out quickly!!!!!");
+ }
+
+} else  {
+    alert('sorry get out qucikly');
+}
