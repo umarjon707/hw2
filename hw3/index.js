79e3680fef0f6ad513817bdd23b118088d7ed200
Umarjon <urr777777@gmail.com>
2024-04-08 22:55:05 +0500

    new project

---
+++ hw3/index.js
@@ -0,0 +1,53 @@
+// const arr = [1,2,3, ' Alex ', ' I am ', [1,2, 'Hello' ], ' 24 ', ' My name is ', [1,[' years old ', ' and ']]]
+
+
+/* 1 method */
+// // let txt = arr[5][2]  + [' my name is Alex and i am 24 years old '] 
+
+/* 2 method */
+
+// let txt = arr[5] [2] + arr[7] + arr[3] + arr[8] [1] [1] + arr[4] + arr[6]+arr[8] [1] [0]
+// console.log(txt);
+// console.log(arr)
+
+
+
+/* if you write numbers of the names (indexes) concole will slice the chosen name    */
+
+// const arr =['Alex', 'Kevin', 'Ronalgo', 'Vini' , 'Haaland']
+// let n = +prompt('write down any number?')
+// if (n===1) {
+//   arr.splice(0,1)
+// } else if (n===2) {
+//     arr.splice(1,1)
+// } else if (n===3) {  
+//     arr.splice(2,1)
+// } else if (n===4) {   
+//     arr.splice(3,1)
+//   } else if (n===5) {   
+//     arr.splice(4,1)
+//   } else {
+//     alert('nooooo')
+//   }
+//   console.log(arr)
+
+
+//   const arr =['Alex', 'Kevin', 'Ronalgo', 'Vini' , 'Haaland']
+//   let n = prompt('write down any name?')
+//   if (n==='Alex') {
+//      console.log(arr.splice(0,1))
+//   } else if (n==="Kevin") {
+//       console.log(arr.splice(1,1))
+//   } else if (n==="Ronaldo") {  
+//       console.log(arr.splice(2,1))
+//   } else if (n==="Vini") {   
+//       console.log(arr.splice(3,1))
+//     } else if (n==="Haaland") {   
+//       console.log(arr.splice(4,1))
+//     } else {
+//       alert('nooooo')
+//     }
+  
+  
+
+
